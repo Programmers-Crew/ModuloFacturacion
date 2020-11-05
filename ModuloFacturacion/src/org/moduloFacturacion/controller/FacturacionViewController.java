9f86df80a503bf48f1bc0409fa3095d4ca8e3eb0
dgonzalez2019203 <dgonzalez-2019203@kinal.edu.gt>
2020-11-04 19:02:03 -0600

    Se resolvio problema en controlador facturacion

--- ModuloFacturacion/src/org/moduloFacturacion/controller/FacturacionViewController.java
+++ ModuloFacturacion/src/org/moduloFacturacion/controller/FacturacionViewController.java
@@ -27,6 +27,7 @@ import javafx.util.Duration;
 import org.controlsfx.control.Notifications;
 import org.moduloFacturacion.bean.AutoCompleteComboBoxListener;
 import org.moduloFacturacion.bean.CambioScene;
+import org.moduloFacturacion.bean.Productos;
 import org.moduloFacturacion.db.Conexion;
 
 
@@ -41,7 +42,7 @@ public class FacturacionViewController implements Initializable {
 
     boolean comprobarCliente = false;
     Notifications noti = Notifications.create();
-    int codigoProducto = 1;
+    int codigoProducto;
     
     
     @FXML
@@ -195,36 +196,38 @@ public class FacturacionViewController implements Initializable {
     }
 
     
-public int buscarCodigoProducto(int codigoProducto){
-         String sql = "{call SpBuscarClientesNit('"+codigoProducto+"')}";
-         
+public int buscarCodigoProducto(String precioProductos){    
         try{
-            PreparedStatement ps = Conexion.getIntance().getConexion().prepareCall(sql);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                          codigoProducto = rs.getInt("productoId");
-                }
-        }catch(SQLException ex){
-            System.out.println(ex);
+            PreparedStatement sp = Conexion.getIntance().getConexion().prepareCall("{call SpBuscarcodigoProducto(?)}");
+            sp.setString(1, precioProductos);
+            ResultSet resultado = sp.executeQuery(); 
+            
+            while(resultado.next()){
+            codigoProducto = resultado.getInt(1);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
         return codigoProducto;
     }
     
     
+    @FXML
     public void buscarPrecioMetodo(){
-         String sql = "{call SpBuscarProductos('"+codigoProducto+"')}";
-         System.out.println(codigoProducto);
-        try{
-            PreparedStatement ps = Conexion.getIntance().getConexion().prepareCall(sql);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                    txtPrecioProducto.setText(rs.getString("productoPrecio"));
-                    
+        if(cmbNombreProducto.getValue()!= ""){
+                try{
+                     PreparedStatement sp = Conexion.getIntance().getConexion().prepareCall("{call SpBuscarProductos(?)}");
+                    sp.setInt(1, buscarCodigoProducto(cmbNombreProducto.getValue()));
+                     ResultSet resultado = sp.executeQuery(); 
+                        while(resultado.next()){
+                            txtPrecioProducto.setText(resultado.getString("productoPrecio"));
+                        }  
+                }catch(Exception e){
+                    e.printStackTrace();
                 }
-        }catch(SQLException ex){
-            System.out.println(ex);
-        }
+            }
     }
+    
     
     @FXML
     private void buscarPrecio(ActionEvent event) {
