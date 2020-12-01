b21533fc5c9fe7e681e334fe5330ce58acb4f0bb
dgonzalez2019203 <dgonzalez-2019203@kinal.edu.gt>
2020-11-30 19:43:28 -0600

    ultimos cambios

--- ModuloFacturacion/src/org/moduloFacturacion/db/Conexion.java
+++ ModuloFacturacion/src/org/moduloFacturacion/db/Conexion.java
@@ -13,7 +13,7 @@ public class Conexion {
     public Conexion(){
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-                conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProgrammersBilling?useSSL=False", "root" , "Fernando2003");
+                conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProgrammersBilling?useSSL=False", "ProgrammersL1" , "ProgrammersLicenciaUno");
                     }catch(ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException e){
                             e.printStackTrace();
                     }
